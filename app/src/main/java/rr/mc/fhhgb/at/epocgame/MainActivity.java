093a82460b701b398b77498d0463208262632fbe
Ralph Rohner <derspringende@gmail.com>
2016-04-28 20:58:15 +0200

    intial test

--- app/src/main/java/rr/mc/fhhgb/at/epocgame/MainActivity.java
+++ app/src/main/java/rr/mc/fhhgb/at/epocgame/MainActivity.java
@@ -1,10 +1,12 @@
 package rr.mc.fhhgb.at.epocgame;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    // random comment by ralph
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
