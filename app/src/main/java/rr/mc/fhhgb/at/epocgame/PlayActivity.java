f278c7245896257604edf7634aa0497772274e25
Ralph Rohner <derspringende@gmail.com>
2016-05-10 14:26:03 +0200

    main view, buttons, imageview, activities 2nd

---
+++ app/src/main/java/rr/mc/fhhgb/at/epocgame/PlayActivity.java
@@ -0,0 +1,13 @@
+package rr.mc.fhhgb.at.epocgame;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+    }
+}
