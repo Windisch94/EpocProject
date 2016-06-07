d1a4a3468d9dbd607e0d10cd1fe3b5fe7bbcf577
Ralph Rohner <derspringende@gmail.com>
2016-06-07 20:08:52 +0200

    updated playActivity

--- app/src/main/java/rr/mc/fhhgb/at/epocgame/AnimatedView.java
+++ app/src/main/java/rr/mc/fhhgb/at/epocgame/AnimatedView.java
@@ -3,9 +3,11 @@ package rr.mc.fhhgb.at.epocgame;
 /**
  * Created by Windisch on 30.05.2016.
  */
+
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Handler;
 import android.util.AttributeSet;
 import android.widget.ImageView;
@@ -21,6 +23,7 @@ public class AnimatedView extends ImageView {
     };
     int x = -1;
     int y = -1;
+    Bitmap smallBall;
     private Context mContext;
     private int xVelocity = 5;
     private Handler h;
@@ -32,21 +35,23 @@ public class AnimatedView extends ImageView {
     }
 
     public void onDraw(Canvas c) {
-        BitmapDrawable ball = (BitmapDrawable) mContext.getResources()
-                .getDrawable(R.drawable.ball);
+        Bitmap ball = BitmapFactory.decodeResource(getResources(), R.drawable.ball);
+        //smallBall = Bitmap.createScaledBitmap(ball, 100, 100, false);
         if (x < 0 && y < 0) {
             x = 0;
-            y = this.getHeight() - this.getHeight() / 3;
+            y = this.getHeight() - this.getHeight() / 5;
         } else {
             if (shouldDraw) {
                 x += xVelocity;
-                if ((x > this.getWidth() - ball.getBitmap().getWidth()) || (x < 0)) {
+                if ((x > this.getWidth() - ball.getWidth()) || (x < 0)) {
                     x = 0;
                 }
             }
         }
 
-        c.drawBitmap(ball.getBitmap(), x, y, null);
+        //funktion unten
+        //ball = EpocQualityView.getResizedBitmap(ball,100,100);
+        c.drawBitmap(ball, 0, 0, null);
         h.postDelayed(r, FRAME_RATE);
     }
 
@@ -55,12 +60,71 @@ public class AnimatedView extends ImageView {
     }
 
     public void reset() {
-        this.x = 0;
-        this.shouldDraw= false;
+        this.shouldDraw = false;
 
     }
 
     public void stopAnimation(Canvas c) {
 
     }
+
 }
+
+    /*
+    public boolean shouldDraw = false;
+    private int screenW;
+    private int screenH;
+
+    private float drawScaleW;
+    private float drawScaleH;
+
+    int moveRate = 10, ballY, ballX;
+
+    private Context myContext;
+
+    private Bitmap ball;
+    private boolean dotGoing = true, gameOver = false;
+
+
+    public AnimatedView(Context context) {
+        super(context);
+        myContext = context;
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        ball = BitmapFactory.decodeResource(myContext.getResources(), R.drawable.ball);
+        screenW = w;
+        screenH = h;
+        drawScaleW = (float) screenW / 800;
+        drawScaleH = (float) screenH / 600;
+        ballY = ball.getHeight();
+        ballX = w/2-(ball.getWidth()/2);
+    }
+
+    public void run() {
+        if (!gameOver) {
+            animateDot();
+            invalidate();
+        }
+    }
+    public void reset() {
+        this.shouldDraw= false;
+
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Paint redPaint = new Paint();
+        redPaint.setColor(Color.RED);
+        canvas.drawBitmap(ball, ballX, ballY, null);
+        run();
+    }
+    private void animateDot(){
+        if (dotGoing) {
+            if(ballY <screenH- ball.getHeight())
+                ballY += moveRate;
+        }
+    }
+}
+*/
--- app/src/main/java/rr/mc/fhhgb/at/epocgame/PlayActivity.java
+++ app/src/main/java/rr/mc/fhhgb/at/epocgame/PlayActivity.java
@@ -3,43 +3,110 @@ package rr.mc.fhhgb.at.epocgame;
 import android.animation.ValueAnimator;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class PlayActivity extends AppCompatActivity {
-    //NUR AUSPROBIERT! KANNST A LÖSCHEN
-    Button button;
+
+    Button buttonMoveBgd;
+    Button buttonMoveBall;
+    Button buttonStart;
     ValueAnimator animator;
     long duration = 10000L;
     int height;
     int width;
     ImageView backgroundOne;
     ImageView backgroundTwo;
+    TextView textViewCountdown;
+    TextView textViewDistance;
+    boolean fast = false;
+    long distance;
+    long startTime;
+    long elapsedTime;
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
-        button = (Button)findViewById(R.id.buttonFaster);
-        button = (Button) findViewById(R.id.buttonMoveBall);
-        button.setOnClickListener(new View.OnClickListener() {
+        textViewCountdown = (TextView) findViewById(R.id.textViewCountdown);
+        textViewDistance = (TextView) findViewById(R.id.textViewDistance);
+
+        buttonMoveBgd = (Button) findViewById(R.id.buttonMoveBgd);
+        buttonMoveBall = (Button) findViewById(R.id.buttonMoveBall);
+        buttonStart = (Button) findViewById(R.id.buttonStart);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //move ball
+                //start timer and game
+                animator.start();
+                startTime = System.currentTimeMillis();
+
+                new CountDownTimer(20000, 1000) {
+
+                    public void onTick(long millisUntilFinished) {
+
+
+                        textViewCountdown.setText("Remaining: " + millisUntilFinished / 1000 + "sec");
+                        distance = (System.currentTimeMillis() - startTime);
+                        //plus move ball factor
+                        textViewDistance.setText("Distance: " + (distance / 1000) + "m");
+
+                    }
+
+                    public void onFinish() {
+                        textViewCountdown.setText("end!");
+                        animator.end();
+
+                    }
+                }.start();
 
             }
         });
-        button.setOnClickListener(new View.OnClickListener() {
+
+        buttonMoveBall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Move Ball
+                //moveBall();
+            }
+        });
+
+
+        buttonMoveBgd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //background animation wird schneller
-                animator.setDuration(duration / 2);
+
+                if (fast == false) {
+                    animator.setDuration(duration / 3);
+                    fast = true;
+                    buttonMoveBgd.setText("slower");
+
+                } else {
+                    animator.setDuration(duration * 3);
+                    fast = false;
+                    buttonMoveBgd.setText("faster");
+                }
 
             }
         });
@@ -50,9 +117,10 @@ public class PlayActivity extends AppCompatActivity {
 
 
         Bitmap bm = ((BitmapDrawable) backgroundOne.getDrawable()).getBitmap();
-        bm = EpocQualityView.getResizedBitmap(bm, dm.heightPixels + 60, dm.widthPixels + 200);
+        bm = EpocQualityView.getResizedBitmap(bm, dm.heightPixels + 40, dm.widthPixels + 200);
         Bitmap bm2 = ((BitmapDrawable) backgroundTwo.getDrawable()).getBitmap();
-        bm2 = EpocQualityView.getResizedBitmap(bm2, dm.heightPixels + 60, dm.widthPixels + 200);
+        bm2 = EpocQualityView.getResizedBitmap(bm2, dm.heightPixels + 40, dm.widthPixels + 200);
+
 
         backgroundOne.setImageBitmap(bm);
         backgroundTwo.setImageBitmap(bm2);
@@ -66,12 +134,56 @@ public class PlayActivity extends AppCompatActivity {
             public void onAnimationUpdate(ValueAnimator animation) {
                 final float progress = (float) animation.getAnimatedValue();
                 final float width = backgroundOne.getWidth();
-                final float translationX = width * progress *-1;
+                final float translationX = width * progress * -1;
                 backgroundOne.setTranslationX(translationX);
                 backgroundTwo.setTranslationX(translationX + width);
+
             }
         });
-        animator.start();
 
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Play Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://rr.mc.fhhgb.at.epocgame/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Play Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://rr.mc.fhhgb.at.epocgame/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
